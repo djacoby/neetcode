73e99b7731139db04e53441398a2c4cd85f14565
David Jacoby <jacoby934@gmail.com>
2025-02-26 21:44:02 -0500

    Leetcode 217 - Contains Duplicate

---
+++ arrays_and_hashing/contains_duplicate.py
@@ -0,0 +1,25 @@
+"""
+Contains Duplicate
+
+Leetcode 217 - https://leetcode.com/problems/contains-duplicate/
+
+Time Complexity - O(n)
+Space Complexity - O(n)
+
+"""
+
+from typing import List
+from utils import LeetTester
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+         return len(set(nums)) < len(nums)
+
+tester = LeetTester()
+sol = Solution()
+
+tester.test(True, sol.containsDuplicate([1, 2, 3, 1]))
+tester.test(False, sol.containsDuplicate([1, 2, 3, 4]))
+tester.test(True, sol.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+
+tester.summary()
